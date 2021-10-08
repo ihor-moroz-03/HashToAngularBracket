94ba1e460989f5b78f4b90a4dae50fa05f9c3f7a
Ihor Moroz <ihor.moroz.17@gmail.com>
2021-10-08 22:18:12 +0300

    Add project files.

---
+++ Formatter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashToAngularBracket
+{
+    static class Formatter
+    {
+        public static string[] Format(string[] text)
+        {
+            var hashIndices = new List<(int, int)>();
+            for (int i = 0; i < text.Length; ++i)
+                for (int j = 0; j < text[i].Length; ++j)
+                {
+                    if (text[i][j] == '#') hashIndices.Add((i, j));
+                }
+
+            //add one bracket to the very end if odd
+            if (hashIndices.Count % 2 == 1)
+                hashIndices.Add((text.Length - 1, text[^1].Length - 1));
+
+            int middleLine = hashIndices[hashIndices.Count / 2].Item1;
+
+            StringBuilder[] builders = new StringBuilder[text.Length];
+            for (int i = 0; i < text.Length; ++i) builders[i] = new StringBuilder(text[i]);
+            for (int i = 0; i < hashIndices.Count / 2; ++i)
+            {
+                builders[hashIndices[i].Item1][hashIndices[i].Item2] = '<';
+            }
+            for (int i = hashIndices.Count / 2; i < hashIndices.Count; ++i)
+            {
+                builders[hashIndices[i].Item1][hashIndices[i].Item2] = '>';
+            }
+
+            string[] result = new string[text.Length];
+            for (int i = 0; i < text.Length; ++i)
+                result[i] = builders[i].ToString();
+
+            return result;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace HashToAngularBracket
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] example = @"##Aut dolores #incidunt consequuntur #qui doloribus eius sit.
+# #Quidem omnis hic #sit et porro qui.
+Recusandae est veniam sed asperiores repellendus. Iure repellendus autem sunt.
+Unde unde omnis nulla laborum. Et voluptates omnis sed dolorum in #eius cupiditate.
+#Cupiditate aut qui dolor.
+Neque repellat accusantium# voluptatem quaerat et velit. Suscipit# cum odit eum veniam repellat et suscipit nemo.
+Quas delectus aliquam fuga dolor iure voluptatem. Dolorem a modi consectetur quidem dicta dolorem.
+####Qui ###animi ratione fugit# ##perferendis soluta quia dolor est. ##Modi est### optio quod.
+Eaque amet dolores facilis impedit. Omnis nobis ad voluptatem.
+Adipisci# sunt nemo beatae illo sed et voluptas beatae.
+#Iste asperiores eum totam#. Cupiditate sunt ut ea dicta nemo ab accusamus facere.".Split('\n');
+
+            Console.Write(string.Join<string>('\n', Formatter.Format(example)));
+            Console.Read();
+        }
+    }
+}
